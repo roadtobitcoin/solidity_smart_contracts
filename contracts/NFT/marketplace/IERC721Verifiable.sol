549c3c715198b879f69554e0afc74e047d5c4b6b
roadtobitcoin <39312905+roadtobitcoin@users.noreply.github.com>
2021-11-04 11:35:30 -0400

    Update IERC721Verifiable.sol

--- contracts/NFT/marketplace/IERC721Verifiable.sol
+++ contracts/NFT/marketplace/IERC721Verifiable.sol
@@ -6,5 +6,5 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 
 interface IERC721Verifiable is IERC721 {
-    function verifyFingerprint(uint256, bytes32) external view returns (bool);
+    function verifyFingerprint(uint256, bytes32) public view returns (bool);
 }
